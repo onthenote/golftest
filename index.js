07225bb643cf467653e16aab73dfd1b189afe342
onthenote <132839066+onthenote@users.noreply.github.com>
2023-05-17 22:14:03 +0900

    first commit
    1. count - api

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+let count = 1000;
+
+app.get('/api/count', (req, res) => {
+  res.json({ count: count });
+});
+
+app.post('/api/count', (req, res) => {
+  count++;
+  res.json({ count: count });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
